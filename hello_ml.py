d1fded49e14cefbea9be402bfc9210912d56dc14
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-23 10:40:49 -0600

    adds algorithms - LogisticRegression, LinearDiscriminantAnalysis, KNeighborsClassifier, DecisionTreeClassifier, GaussianNB, SVC

--- hello_ml.py
+++ hello_ml.py
@@ -54,5 +54,27 @@ y = array[:, 4]
 X_train, X_validation, Y_train, Y_validation = train_test_split(
     X, y, test_size=0.20, random_state=1)
 
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(
+    solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(
+        model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+
 # Tutorial
 # https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
