437f5732139a1fbac38a414e115e01d4247fb684
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-23 10:50:35 -0600

    adds evaluation of predictions

--- hello_ml.py
+++ hello_ml.py
@@ -56,24 +56,24 @@ X_train, X_validation, Y_train, Y_validation = train_test_split(
 
 
 # Spot Check Algorithms
-models = []
-models.append(('LR', LogisticRegression(
-    solver='liblinear', multi_class='ovr')))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC(gamma='auto')))
-# evaluate each model in turn
-results = []
-names = []
-for name, model in models:
-    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-    cv_results = cross_val_score(
-        model, X_train, Y_train, cv=kfold, scoring='accuracy')
-    results.append(cv_results)
-    names.append(name)
-    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+# models = []
+# models.append(('LR', LogisticRegression(
+#     solver='liblinear', multi_class='ovr')))
+# models.append(('LDA', LinearDiscriminantAnalysis()))
+# models.append(('KNN', KNeighborsClassifier()))
+# models.append(('CART', DecisionTreeClassifier()))
+# models.append(('NB', GaussianNB()))
+# models.append(('SVM', SVC(gamma='auto')))
+# # evaluate each model in turn
+# results = []
+# names = []
+# for name, model in models:
+#     kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+#     cv_results = cross_val_score(
+#         model, X_train, Y_train, cv=kfold, scoring='accuracy')
+#     results.append(cv_results)
+#     names.append(name)
+#     print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
 
 # Compare Algorithms
 # pyplot.boxplot(results, labels=names)
@@ -84,6 +84,11 @@ for name, model in models:
 model = SVC(gamma='auto')
 model.fit(X_train, Y_train)
 predictions = model.predict(X_validation)
+
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
 
 # Tutorial
 # https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
