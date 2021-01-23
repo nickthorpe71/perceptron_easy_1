0f8aec353c9b7aa99894e6429184d6d3f9a2f4bc
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-23 08:35:37 -0600

    adds function sigmoid with 100k iterations for training

--- perceptron.py
+++ perceptron.py
@@ -5,6 +5,10 @@ def sigmoid(x):
     return x / (1 + np.exp(-x))
 
 
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+
 training_inputs = np.array([[0, 0, 1],
                             [1, 1, 1],
                             [1, 0, 1],
@@ -19,11 +23,20 @@ synaptic_weights = 2 * np.random.random((3, 1)) - 1
 print('Random starting synaptic weights: ')
 print(synaptic_weights)
 
-for iteration in range(1):
+for iteration in range(100000):
 
     input_layer = training_inputs
 
     outputs = sigmoid(np.dot(input_layer, synaptic_weights))
+
+    error = training_outputs - outputs
+
+    adjustments = error * sigmoid_derivative(outputs)
+
+    synaptic_weights += np.dot(input_layer.T, adjustments)
+
+print('Synaptic weights after training')
+print(synaptic_weights)
 
 print('Outputs after training: ')
 print(outputs)
